5346561e6e420e1b054124364b71c1e162b5bee1
yordan-vladov <jordan0vladov@gmail.com>
2025-02-06 16:06:55 +0200

    Added refactoring

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,6 +1,6 @@
-# test_fizzbuzz.py
 import pytest
 from fizzbuzz import fizzbuzz  # Import function (yet to be implemented)
 
 def test_returns_number():
-    assert fizzbuzz(1) == 1  # Expect 1 to return 1
+	simple_number = 1 # avoid magic numbers
+	assert fizzbuzz(simple_number) == simple_number # Expect 1 to return 1

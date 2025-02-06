9355a1580aa87dbd221e4b16e22660de8596b56d
yordan-vladov <jordan0vladov@gmail.com>
2025-02-06 15:58:02 +0200

    Initial commit

---
+++ fizzbuzz.py
@@ -0,0 +1,4 @@
+# fizzbuzz.py
+
+def fizzbuzz(n):
+    return n  # Simple return to pass the first test
---
+++ test_fizzbuzz.py
@@ -0,0 +1,6 @@
+# test_fizzbuzz.py
+import pytest
+from fizzbuzz import fizzbuzz  # Import function (yet to be implemented)
+
+def test_returns_number():
+    assert fizzbuzz(1) == 1  # Expect 1 to return 1

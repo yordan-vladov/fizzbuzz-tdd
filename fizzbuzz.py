82f9ea007c6d644832d511272f5a011e311f1a1a
DanielDimitrov46 <dani.dimitrov.snack9@gmail.com>
2025-02-14 08:41:54 +0200

    Tests passed

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,10 @@
 # fizzbuzz.py
 
 def fizzbuzz(n):
+    if n % 3 == 0 and n % 5 == 0:
+        return "FizzBuzz"
+    elif n % 3 == 0:
+        return "Fizz"
+    elif n % 5 == 0:
+        return "Buzz"
     return n
